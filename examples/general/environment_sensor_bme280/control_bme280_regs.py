f0a823ae219bb418f026c3e8a4aa7b07aaa9f95f
Dan Shemesh <dan@jumper.io>
2017-12-26 18:42:16 +0200

    bme example

--- examples/general/environment_sensor_bme280/control_bme280_regs.py
+++ examples/general/environment_sensor_bme280/control_bme280_regs.py
@@ -13,7 +13,8 @@ fw_bin = os.path.join(dir, 'pca10040', 'blank', 'armgcc', '_build', 'nrf52832_xx
 
 vlab = Vlab(working_directory=dir, print_uart=False)
 vlab.load(fw_bin)
-vlab.start()
+vlab.run_for_ms(500)
+vlab.resume()
 print('Virtual device is running')
 
 print('Reading chip ID from register {}'.format(hex(CHIP_ID_REGISTER)))
@@ -27,18 +28,12 @@ print('Reading chip ID from register {}'.format(hex(CHIP_ID_REGISTER)))
 chip_id = vlab.BME280.get_register_value(CHIP_ID_REGISTER)
 print('Chip id is: {}'.format(hex(chip_id)))
 
-print("Pausing the system")
 vlab.pause()
 for i in range(10):
-    print("Reading temperature")
     temperature_lsb = vlab.BME280.get_register_value(TEMP_LSB_REGISTER)
     temperature_msb = vlab.BME280.get_register_value(TEMP_MSB_REGISTER)
     temperature = (temperature_msb << 1) | temperature_lsb
     print('Raw data in temperature  register is: {}'.format(temperature))
-
-    print('Running the system for 500ms')
-    vlab.run_for_ms(500)
-    sleep(0.3)
-    print('System is paused')
+    vlab.run_for_ms(1000)
 
 vlab.stop()
