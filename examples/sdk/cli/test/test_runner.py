bfe52f56c88b5dc9725e86de2fa650950e44196e
Dan Shemesh <dansheme@users.noreply.github.com>
2018-02-08 11:40:19 +0200

    New (#1)
    * merge all repo to one
    
    * merge
    
    * wait_until_uart_recieve works
    
    * wait_until_uart_receive done
    
    * pull request fixes
    
    * removed old file
    
    * add gpio polling
    
    * add get device time to example
    
    * fixes after code review
    
    * add button on/off
    
    * removed redundatnt files

--- examples/sdk/cli/test/test_runner.py
+++ examples/sdk/cli/test/test_runner.py
@@ -19,11 +19,11 @@ class TestCLI(unittest.TestCase):
 
     def test_cli(self):
         self.vlab.uart.write(b'log\r\n')
-        self.vlab.uart.wait_until_uart_receives(b'status   :Logger status')
+        self.vlab.uart.wait_until_uart_receives(b'status   :Logger status', 500)
         self.vlab.uart.write(b'python\r\n')
-        self.vlab.uart.wait_until_uart_receives(b'Nice joke ;)')
+        self.vlab.uart.wait_until_uart_receives(b'Nice joke ;)', 500)
         self.vlab.uart.write(b'history\r\n')
-        self.vlab.uart.wait_until_uart_receives(b'[  2] history')
+        self.vlab.uart.wait_until_uart_receives(b'[  2] history', 500)
 
 
 if __name__ == '__main__':
